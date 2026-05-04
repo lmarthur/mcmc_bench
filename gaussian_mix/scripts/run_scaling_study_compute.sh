cda8dac7e44d23a67d979a24a17b5e16327bf0db
Lucas Arthur <lmarthur@mit.edu>
2026-05-04 18:14:05 -0400

    Updated GM runs

--- gaussian_mix/scripts/run_scaling_study_compute.sh
+++ gaussian_mix/scripts/run_scaling_study_compute.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1
 #SBATCH -n 8
 #SBATCH --mem-per-cpu=4G
-#SBATCH -p mit_preemptable
+#SBATCH -p mit_normal_gpu
 #SBATCH -G l40s:1
 #SBATCH -o gaussian_mix/output/scaling_study_compute/slurm_%j.out
 #SBATCH -e gaussian_mix/output/scaling_study_compute/slurm_%j.err
